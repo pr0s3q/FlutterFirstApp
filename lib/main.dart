4b97b4c0e9d8909a3de36e42ebef3d069923a34a
pr0s3q <pr0s3q@gmail.com>
2021-12-28 10:52:34 +0100

    Create function to press button

--- lib/main.dart
+++ lib/main.dart
@@ -2,33 +2,52 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Junior Program Tester'];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Flutter First Test App'),
-          ),
-          body: Column(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: RaisedButton(
-                  onPressed: () {},
-                  child: Text('Add language'),
-                ),
+        appBar: AppBar(
+          title: Text('Flutter First Test App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _products.add('Mid Program Tester');
+                  });
+                },
+                child: Text('Add language'),
               ),
-              Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/c#.jpg'),
-                    Text('.net devs paradise')
-                  ],
-                ),
-              ),
-            ],
-          )),
+            ),
+            Column(
+              children: _products
+                  .map(
+                    (element) => Card(
+                      child: Column(
+                        children: <Widget>[
+                          Image.asset('assets/c#.jpg'),
+                          Text(element)
+                        ],
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
