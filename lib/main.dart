0fd5148264605ada8af12e29442b0925b8446990
pr0s3q <pr0s3q@gmail.com>
2021-12-28 10:34:39 +0100

    Add button

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter First Test App'),
-        ),
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Image.asset('assets/c#.jpg'),
-              Text('.net devs paradise')
-            ],
+          appBar: AppBar(
+            title: Text('Flutter First Test App'),
           ),
-        ),
-      ),
+          body: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Add language'),
+                ),
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset('assets/c#.jpg'),
+                    Text('.net devs paradise')
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
